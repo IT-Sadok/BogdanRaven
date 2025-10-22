ceb389fa74c2be8577998e7e12a5225a3e78bfe0
BogdanRaven <55992260+BogdanRaven@users.noreply.github.com>
2025-10-23 02:06:15 +0300

    fixed library state provider

--- LibraryApp/Services/LibraryStateProvider.cs
+++ LibraryApp/Services/LibraryStateProvider.cs
@@ -5,6 +5,13 @@ namespace LibraryApp.Services;
 public class LibraryStateProvider : ILibraryStateProvider
 {
     public LibraryState LibraryState { get; private set; }
+    
+    public LibraryStateProvider(ISaveLoadService<LibraryState> saveLoadService)
+    {
+        LibraryState = saveLoadService.IsSaveExistsAsync().Result
+            ? saveLoadService.LoadAsync().Result!
+            : new LibraryState();
+    }
     public void SetState(LibraryState libraryState)
     {
         LibraryState = libraryState;
