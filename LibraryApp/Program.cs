2c8d9b35b2bb954fef7408d4122effba3aec4e54
BogdanRaven <55992260+BogdanRaven@users.noreply.github.com>
2025-10-23 02:33:44 +0300

    develop entry point

---
+++ LibraryApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using LibraryApp.Menu;
+using LibraryApp.Repositories;
+using LibraryApp.Repositories.Interfaces;
+using LibraryApp.Services;
+using LibraryApp.Services.Interfaces;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((context, services) =>
+    {
+        var filePath = "books.json";
+        services.AddSingleton<ISaveLoadService<LibraryState>>(sp =>
+            new JsonSaveLoadService<LibraryState>(filePath));
+
+        services.AddSingleton<ILibraryStateProvider, LibraryStateProvider>();
+
+        services.AddScoped<IBookRepository, BookRepository>();
+
+        services.AddScoped<ILibraryService, LibraryService>();
+
+        services.AddScoped<ConsoleMenu>();
+    })
+    .Build();
+
+var menu = host.Services.GetRequiredService<ConsoleMenu>();
+await menu.StartAsync();
